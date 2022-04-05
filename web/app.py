010dd8cb68a6035e2ff32ded50801800b7535a9e
jjavierf64 <jjavierf64@estudiantec.cr>
2022-04-04 19:25:36 -0600

    First app config

--- web/app.py
+++ web/app.py
@@ -3,5 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/home")
+
+def home():
+    return "<h1 style='color: darkblue;'> Hello, World!</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1 style='color: darkblue;'> About Page</h1>"
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

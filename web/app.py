5ab2c90bb7a4b19d06dc35a3db015f72bf1d8247
jjavierf64 <jjavierf64@estudiantec.cr>
2022-04-04 20:32:45 -0600

    App base creation and basic structure

--- web/app.py
+++ web/app.py
@@ -1,17 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author':'Jav',
+        'title': 'BlogPost',
+        'content': 'Lorem ipsum',
+        'date_posted': 'April 04, 2022'
+    },
+
+    {
+        'author':'Jose',
+        'title': 'Another BlogPost',
+        'content': 'Lorem ipsum 22222',
+        'date_posted': 'April 05, 2022'
+    }
+
+
+]
+
+
 
 @app.route("/")
 @app.route("/home")
 
 def home():
-    return "<h1 style='color: darkblue;'> Hello, World!</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1 style='color: darkblue;'> About Page</h1>"
+    return render_template('about.html', title='About')
+
 
 
 
